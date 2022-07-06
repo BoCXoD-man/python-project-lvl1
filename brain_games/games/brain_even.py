f4e13fe59ce0aa64b60cb39fc564b7dd90c6efd2
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-06 14:06:32 +0400

    update code brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,36 +1,18 @@
 """Brain even game functions."""
 
-import prompt
-from brain_games.engine import generate_number, welcome
+from brain_games.engine import generate_number
+
+DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
 
 
-def generation_question():
-    """Question"""
-    qwest = generate_number()
-    print(f'Question: {qwest}')
-    return 'yes' if qwest % 2 == 0 else 'no'
+def make_question():
+    """Generate question."""
+    number = generate_number()
+    question = f'Question: {number}'
+    answer = correct_answer(number)
+    return (question, answer)
 
 
-def answer():
-    """Answer"""
-    ent = prompt.string('Your answer: ')
-    return ent
-
-
-def brain_even():
-    """Brain_even game"""
-    name = welcome()
-    count = 0
-    while True:
-        question = generation_question()
-        ent = answer()
-        if question == ent:
-            print('Correct!')
-            count += 1
-        else:
-            print(f"'{ent}' is wrong answer ;(. Correct answer was '{question}"
-                  f"'.\nLet's try again, {name}")
-            break
-        if count == 3:
-            print(f'Congratulations, {name}!')
-            break
+def correct_answer(number):
+    """Return True answer."""
+    return 'no' if number % 2 else 'yes'
