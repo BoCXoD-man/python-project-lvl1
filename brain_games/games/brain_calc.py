259716ade1bf920e69fb8966c8cde429ceff5758
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-06 15:22:31 +0400

    add new game - calc

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,31 @@
+import operator
+from random import choice
+
+from brain_games.engine import generate_number
+
+DESCRIPTION = 'What is the result of the expression?'
+
+operation = {
+    '+': operator.add(),
+    '-': operator.sub(),
+    '*': operator.mul()
+}
+
+
+def generate_operation():
+    """Generate random choice operations from the dictionary"""
+    return choice(list(operation.keys()))
+
+
+def make_question():
+    """Generate question."""
+    num1, num2 = generate_number(), generate_number()
+    oper = generate_operation()  # oper - operation
+    question = f'Question: {num1} {oper} {num2}'
+    answer = correct_answer(num1, num2, oper)
+    return (question, answer)
+
+
+def correct_answer(num1, num2, oper):
+    """Return True answer."""
+    return str(operation[oper](num1, num2))
