228ec834f0c880292d58247c3e3cedde952f2178
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-08 12:26:54 +0400

    fix bugs

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -18,4 +18,4 @@ def correct_answer(num1, num2):
     for i in range(1, num2 + 1):
         if num1 % i == 0 and num2 % i == 0:
             list_com_div.append(i)
-    return max(list_com_div)
+    return str(max(list_com_div))
