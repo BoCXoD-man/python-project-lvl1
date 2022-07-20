f08e5d65790f21da31f7a4ff11b3a768fbba0acd
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-20 19:15:54 +0400

    Fixed 2 remarks of the mentor

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
-""" Main Module"""
-from brain_games.engine import start
+#!/usr/bin/env python3
+
+"""A greeting to the user."""
+
+from brain_games.cli import welcome_user
 
 
 def main():
-    """ Main program"""
-    start()
+    """Main Program"""
+    welcome_user()
 
 
 if __name__ == '__main__':
