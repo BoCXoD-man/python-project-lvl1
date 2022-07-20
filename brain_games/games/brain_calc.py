14941152c36e7aefe50f49ba05ec5a5987a5b90a
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-20 20:42:13 +0400

    fix bugs

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -17,5 +17,5 @@ def make_question():
     num1, num2 = generate_number(), generate_number()
     operation = choice(list(operations.keys()))  # oper - operation
     question = f'Question: {num1} {operation} {num2}'
-    answer = str(operations[operator](num1, num2))
+    answer = str(operations[operation](num1, num2))
     return (question, answer)
