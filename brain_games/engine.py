54ee2ed4e56804c04528fb04abb8b78da3b0a2b6
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-23 15:15:20 +0400

    fix optimization engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,23 +8,20 @@ NUMBER_OF_ROUNDS = 3
 def engine(game):
     """Start game."""
     print('Welcome to the Brain Games!')
-    if game:
-        print(game.DESCRIPTION)
+    print(game.DESCRIPTION)
     user_name = prompt.string('May I have your name? ')
-    greeting = f'Hello, {user_name}!'
-    print(greeting)
-    if game:
-        for _ in range(NUMBER_OF_ROUNDS):
-            question, correct_answer = game.get_question_and_answer()
-            print(question)
-            users_answer = prompt.string('Your answer: ')
-            if users_answer == correct_answer:
-                print('Correct!')
-                continue
-            show_game_over(users_answer, correct_answer, user_name)
-            break
-        else:
-            print(f'Congratulations, {user_name}!')
+    print(f'Hello, {user_name}!')
+    for _ in range(NUMBER_OF_ROUNDS):
+        question, correct_answer = game.get_question_and_answer()
+        print(question)
+        users_answer = prompt.string('Your answer: ')
+        if users_answer == correct_answer:
+            print('Correct!')
+            continue
+        show_game_over(users_answer, correct_answer, user_name)
+        break
+    else:
+        print(f'Congratulations, {user_name}!')
 
 
 def show_game_over(users_answer, correct_answer, user_name):
@@ -32,5 +29,5 @@ def show_game_over(users_answer, correct_answer, user_name):
     print(f"'{0}' is wrong answer ;(. Correct answer was '{1}'".format(
         users_answer,
         correct_answer,
-        ))
+    ))
     print(f"Let's try again, {user_name}!")
