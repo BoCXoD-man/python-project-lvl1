39ea8837a52d2826b638b360a66c61f03de762e4
BoCXoD-man <an_manaev1988@mail.ru>
2022-07-23 16:14:04 +0400

    fix progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -6,11 +6,11 @@ DESCRIPTION = 'What number is missing in the progression?'
 def get_question_and_answer():
     """Generate question."""
     progression_length = randint(5, 11)
-    start, step = randint(1, 20), randint(1, 10)
+    start, step = randint(1, 30), randint(1, 10)
     progression = list(range(start, progression_length * step + start, step))
 
     answer = choice(progression)
-    question = 'Question:' + ' '.join(
+    question = 'Question: ' + ' '.join(
         '..' if number == answer else str(number) for number in progression
     )
     return question, str(answer)
